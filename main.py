e943c2060dd018dce75bdb7d8b17270551a1e362
xu3cl40122 <xu3cl40122@gmail.com>
2018-05-03 23:10:01 +0800

    maskCollision

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ clock = pygame.time.Clock()
 bg = Object('./Asset1_ball/bg.png',0,0)
 coin = Object('./Asset1_ball/coin.png', 200, 50)
 p1 = Player('./Asset1_ball/sprite1.png',100,150,0)
-
+p2 = Player('./Asset1_ball/sprite2.png',300,150,0)
 running = True
 while running:
     clock.tick(30)
     bg.blit_me(screen)
     coin.blit_me(screen)
     p1.checkUserControl(pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT)
+    p2.checkUserControl(pygame.K_w, pygame.K_s, pygame.K_a, pygame.K_d)
+    p1.checkCollisionCoin(coin)
+    p2.checkCollisionCoin(coin)
     p1.blit_me(screen)
+    p2.blit_me(screen)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- object.py
+++ object.py
@@ -5,30 +5,36 @@ class Player():
     def __init__(self, pic, x, y, score):
         self.canvas = pygame.image.load(pic)
         self.canvas.convert()
-        self.x = x
-        self.y = y
+        # 依據圖片創建rect(hitbox)
+        self.rect = self.canvas.get_rect()
+        self.mask = pygame.mask.from_surface(self.canvas)
+        #為 rect 設定座標，便於操控上下左右
+        self.rect.left, self.rect.top = x,y
         self.score = score
 
     def blit_me(self, screen):
-        screen.blit(self.canvas, [self.x, self.y])
+        screen.blit(self.canvas, [self.rect.left, self.rect.top])
 
     def checkUserControl(self, up, down, left, right):
         keys = pygame.key.get_pressed()
         if keys[up]:
-            self.y -= 5
+            self.rect.top -= 5
         if keys[down]:
-            self.y += 5
+            self.rect.top += 5
         if keys[left]:
-            self.x -= 5
+            self.rect.left -= 5
         if keys[right]:
-            self.x += 5
-
+            self.rect.left += 5 
+    def checkCollisionCoin(self,other) :
+        if pygame.sprite.collide_mask(self, other):
+            print("Collision occurred")
 class Object():
     def __init__(self, pic, x, y):
         self.canvas = pygame.image.load(pic)
         self.canvas.convert()
-        self.x = x
-        self.y = y
-
+        self.rect = self.canvas.get_rect()
+        self.rect.left = x
+        self.rect.top = y
+        self.mask = pygame.mask.from_surface(self.canvas)
     def blit_me(self, screen):
-        screen.blit(self.canvas, [self.x, self.y])
+        screen.blit(self.canvas, [self.rect.left, self.rect.top])
