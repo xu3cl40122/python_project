96a2dbb339d8dbea5ee5fb96710f0485e3df8ce6
xu3cl40122 <xu3cl40122@gmail.com>
2018-05-04 20:56:05 +0800

    addrocks

--- turtle/main.py
+++ turtle/main.py
@@ -5,7 +5,7 @@ from object import*
 
 pygame.init()
 bg_size = [800, 600]
-move_size = [780,580]
+move_size = [770,570]
 screen = pygame.display.set_mode(bg_size)
 pygame.display.set_caption("mySecondGame")
 clock = pygame.time.Clock()
@@ -14,7 +14,10 @@ bg = Object('./Asset1_ball/space800_800.jpg',0,0)
 coin = Object('./Asset1_ball/coin.png', 400, 300)
 p1 = Player('./Asset1_ball/sprite1.png',100,150,0,move_size)
 p2 = Player('./Asset1_ball/sprite2.png',300,150,0,move_size)
-rock = Rock('./Asset1_ball/rock.png', 60,60,[bg_size[0]+100,bg_size[1]+100])
+rock_list = []
+for i in range(0,6):
+    rock = Rock('./Asset1_ball/rock.png', 60,60,[bg_size[0]+100,bg_size[1]+100])
+    rock_list.append(rock)
 running = True
 while running:
     clock.tick(30)
@@ -25,12 +28,14 @@ while running:
     p1.checkCollisionCoin(coin)
     p2.checkCollisionCoin(coin)
     p1.checkCollideOther(p2)
-    p1.checkCollideRock(rock)
-    p2.checkCollideRock(rock)
-    rock.move()
+    for t in range(len(rock_list)):
+        p1.checkCollideRock(rock_list[t])
+        p2.checkCollideRock(rock_list[t])
+    for  j in range(len(rock_list)):
+        rock_list[j].move()
+        rock_list[j].blit_me(screen)
     p1.blit_me(screen)
     p2.blit_me(screen)
-    rock.blit_me(screen)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
