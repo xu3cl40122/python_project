ebe0152309dc25af19460ebbaffbb47b716c38b6
xu3cl40122 <xu3cl40122@gmail.com>
2018-05-10 15:15:18 +0800

    can_match

--- checkImg/main.py
+++ checkImg/main.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from os import walk
-
+from os import listdir
+from os.path import isfile, isdir, join
+tr_id = '155'
 # 指定要列出所有檔案的目錄
-mypath = "../pyGame"
+mypath = './img/155'
 
-# 遞迴列出所有子目錄與檔案
-for root, dirs, files in walk(mypath):
-  print("路徑：", root)
-  print("  目錄：", dirs)
-  print("  檔案：", files)
+# 取得所有檔案與子目錄名稱
+files = listdir(mypath)
+
+# 以迴圈處理
+for f in files:
+  # 產生檔案的絕對路徑
+  fullpath = join(mypath, f)
+  # 判斷 fullpath 是檔案還是目錄
+  if isfile(fullpath):
+    print("檔案：", f)
+  elif isdir(fullpath):
+    print("目錄：", f)
--- checkImg/re.py
+++ checkImg/re.py
@@ -3,11 +3,34 @@
 from openpyxl import load_workbook
 from os import listdir
 from os.path import isfile, isdir, join
+import re
+
+
+def findImg(re_rule, files):
+    for j in range(len(files)):
+           match_object = re.match(re_rule, files[j])
+           if match_object != None:
+               print(match_object)
+               
+
 
 wb = load_workbook(filename='./test.xlsx')
 ws = wb['ts1']
-for i in range(1,94) :
-    tr_id = str(ws.cell(row=i, column=2).value)
-    # 指定要列出所有檔案的目錄
-    mypath = "./img" + tr_id + "/05"
-    print(mypath)
+tr_id = '0'
+#依據 excel 的 row 跑
+for r in range(1,94) :
+    #判斷是否換一條步道
+    if(tr_id != str(ws.cell(row=r, column=2).value)) :
+        tr_id = str(ws.cell(row=r, column=2).value)
+        print('change')
+        mypath = "./img/" + tr_id + "/05"
+        # 取得所有檔案與子目錄名稱
+        files = listdir(mypath)
+        print(files)
+        imgToFind = str(ws.cell(row=r, column=3).value)
+        findImg(imgToFind,files)
+    else :
+        print('same')
+        imgToFind = str(ws.cell(row=r, column=3).value)
+        findImg(imgToFind,files)
+
