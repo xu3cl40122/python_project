4084296c245a4d7edc0ee3327be4af17f71cd705
xu3cl40122 <xu3cl40122@gmail.com>
2018-05-11 10:50:53 +0800

    add_except

--- checkImg/re.py
+++ checkImg/re.py
@@ -9,22 +9,23 @@ import re
 
 def findImg(re_rule, files):
     i = 0
-    while i < len(files) :
+    isFound = False
+    while i < len(files):
         isFound = False
         match_object = re.match(re_rule, files[i], re.IGNORECASE)
         if match_object != None:
                print(match_object)
                isFound = True
                break
-        i +=1
-    if isFound == False :
-        print('------------'+ re_rule + ' not found -------------')
+        i += 1
+    if isFound == False:
+        print('------------' + re_rule + ' not found -------------')
         return False
     return True
 
 
 def writeOutput(isFound, row, tr_id, imgToFind):
-    if isFound :
+    if isFound:
         ws_w.cell(row=row, column=3).value = 'o'
     else:
         ws_w.cell(row=row, column=3).value = 'not found'
@@ -37,32 +38,46 @@ wb_write = Workbook()
 ws_w = wb_write.active
 ws_w.title = "output"
 # init excel to read
-wb_read = load_workbook(filename='./test.xlsx')
+wb_read = load_workbook(filename='./landscape.xlsx')
 ws_r = wb_read['ts1']
 
 
 # --- main ---  依據 excel 的 row 跑
-def main(subFile = '/05'):
+def main(subFile='/05', line=1228, outputFile='output_2.xlsx'):
     tr_id = '0'
-    for r in range(1,1228) :
+    for r in range(1, line):
+        # if 搜尋的怪怪的
+        if (len(str(ws_r.cell(row=r, column=3).value)) < 3) or str(ws_r.cell(row=r, column=3).value) == 'None':
+            ws_w.cell(row=r, column=3).value = 'target??'
+            continue
+        if len(str(ws_r.cell(row=r, column=2).value)) < 1 or str(ws_r.cell(row=r, column=3).value) == 'None':
+            ws_w.cell(row=r, column=3).value = 'null line'
+            continue
         #判斷是否換一條步道
-        if(tr_id != str(ws_r.cell(row=r, column=2).value)) :
+        if(tr_id != str(ws_r.cell(row=r, column=2).value)):
             tr_id = str(ws_r.cell(row=r, column=2).value)
+            if tr_id == 'None':
+                print('----- get none ---')
+                continue
             print('change'+tr_id)
             # 依 loop 更換路徑
             mypath = "./" + tr_id + subFile
             # 取得路徑下所有檔案與子目錄名稱
-            files = listdir(mypath)
-            print(files)
-            imgToFind = str(ws_r.cell(row=r, column=3).value)
-            p_isFound = findImg(imgToFind,files)
-            writeOutput(p_isFound, r, tr_id ,imgToFind)
-
-        else :
+            try:
+                files = listdir(mypath)
+                print(files)
+            except FileNotFoundError:
+                ws_w.cell(row=r, column=3).value = 'error'
             imgToFind = str(ws_r.cell(row=r, column=3).value)
             p_isFound = findImg(imgToFind, files)
             writeOutput(p_isFound, r, tr_id, imgToFind)
 
-    wb_write.save('output.xlsx')
+        else:
+            imgToFind = str(ws_r.cell(row=r, column=3).value)
+            p_isFound = findImg(imgToFind, files)
+            writeOutput(p_isFound, r, tr_id, imgToFind)
 
-main()
+    wb_write.save(outputFile)
+
+
+main('/02', 352)
